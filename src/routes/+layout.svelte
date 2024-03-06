53e7ed901c2040143ad87bd88ab177adbe07f3f0
mou <detective0218gm@gmail.com>
2024-03-06 23:02:46 +0900

    Add vite-pwa

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { pwaInfo } from 'virtual:pwa-info';
+
+	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+</script>
+
+<svelte:head>
+	{@html webManifestLink}
+</svelte:head>
+
+<slot />
